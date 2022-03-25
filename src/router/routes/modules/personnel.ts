31c8ad2e243ef5e3a2385ca55b7b17cfd7112c6c
“Oreolv” <455001000@qq.com>
2022-03-25 09:58:46 +0800

    feat: 创建居民管理路由

---
+++ src/router/routes/modules/personnel.ts
@@ -0,0 +1,27 @@
+import type { AppRouteModule } from '/@/router/types';
+// import { RoleEnum } from '/@/enums/roleEnum';
+import { LAYOUT } from '/@/router/constant';
+
+const dashboard: AppRouteModule = {
+  path: '/personnel',
+  name: 'personnel',
+  component: LAYOUT,
+  redirect: '/personnel/resident',
+  meta: {
+    icon: 'ion:people',
+    title: '防疫管理',
+    orderNo: 100000,
+  },
+  children: [
+    {
+      path: 'resident',
+      name: 'ResidentManagement',
+      component: () => import('/@/views/personnel/resident/index.vue'),
+      meta: {
+        title: '居民管理',
+      },
+    },
+  ],
+};
+
+export default dashboard;
