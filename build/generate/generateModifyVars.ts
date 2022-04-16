94fccac1fa5da521b88a17ae1019dbccb2240af1
“Oreolv” <455001000@qq.com>
2022-04-16 12:14:51 +0800

    style: 修改按钮颜色

--- build/generate/generateModifyVars.ts
+++ build/generate/generateModifyVars.ts
@@ -25,8 +25,8 @@ export function generateModifyVars(dark = false) {
     ...primaryColorObj,
     'info-color': primary,
     'processing-color': primary,
-    'success-color': '#55D187', //  Success color
-    'error-color': '#ED6F6F', //  False color
+    'success-color': '#52C41A', //  Success color
+    'error-color': '#FF4D4F', //  False color
     'warning-color': '#EFBD47', //   Warning color
     //'border-color-base': '#EEEEEE',
     'font-size-base': '14px', //  Main font size
