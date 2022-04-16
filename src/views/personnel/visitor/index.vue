3d9497e3b3023bf405db432ca7effdf9d83b2b8d
“Oreolv” <455001000@qq.com>
2022-04-16 12:08:40 +0800

    feat: 更改访客审批首页视图

--- src/views/personnel/visitor/index.vue
+++ src/views/personnel/visitor/index.vue
@@ -8,10 +8,6 @@
               label: '查看详情',
               onClick: handleInfo.bind(null, record),
             },
-            {
-              label: '审批用户',
-              onClick: handleEdit.bind(null, record),
-            },
           ]"
         />
       </template>
@@ -19,28 +15,23 @@
   </div>
 </template>
 <script lang="ts" setup>
-  import { useModal } from '/@/components/Modal';
-  import { columns, searchFormSchema } from './data';
+  import { columns } from './data';
   import { getVisitorList } from '/@/api/personnel/visitor';
   import { BasicTable, useTable, TableAction } from '/@/components/Table';
   import { useGo } from '/@/hooks/web/usePage';
 
   const go = useGo();
 
-  const [registerTable, { reload }] = useTable({
+  const [registerTable] = useTable({
     title: '居民列表',
     api: getVisitorList,
     columns,
-    formConfig: {
-      labelWidth: 120,
-      schemas: searchFormSchema,
-    },
     striped: false,
     bordered: true,
     showIndexColumn: true,
     canResize: false,
     actionColumn: {
-      width: 200,
+      width: 100,
       title: '操作',
       dataIndex: 'action',
       slots: { customRender: 'action' },
@@ -48,18 +39,7 @@
     },
   });
 
-  function handleEdit() {
-    // openModal(true, {
-    //   record,
-    //   isUpdate: true,
-    // });
-  }
-
   function handleInfo(record: Recordable) {
     go('/personnel/visitor_detail/' + record.id);
-  }
-
-  function handleSuccess() {
-    reload();
   }
 </script>
