3c4505f9aad418523b793e5c7631fbff9c75109b
“Oreolv” <455001000@qq.com>
2022-04-20 17:57:58 +0800

    feat: 异常报备接口

---
+++ src/api/personnel/model/abnormalModel.ts
@@ -0,0 +1,19 @@
+import { TimeStamp, BasicApplyResult } from '/@/api/model/baseModel';
+import { StatusEnum } from '/@/enums/residentEnum';
+
+export interface AbnormalItem extends TimeStamp, BasicApplyResult {
+  temperature: string;
+  diagnosis: StatusEnum;
+  contact: StatusEnum;
+  symptom: StatusEnum;
+  hospital: string;
+}
+
+export interface UpdateAbnormalParams {
+  id: number;
+  status: number;
+  description: string;
+}
+
+export type GetAbnormalListResultModel = AbnormalItem[];
+export type GetAbnormalDetailResultModel = AbnormalItem;
