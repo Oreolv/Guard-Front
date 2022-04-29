ac9a5aa0b9120329be89751d3aba2e0c6704f8eb
“Oreolv” <455001000@qq.com>
2022-04-29 20:34:45 +0800

    feat: 物资申请导出

--- src/views/personnel/material/index.vue
+++ src/views/personnel/material/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="m-4">
     <BasicTable @register="registerTable">
+      <template #toolbar>
+        <a-button @click="customHeader"> 导出列表 </a-button>
+      </template>
       <template #action="{ record }">
         <TableAction
           :actions="[
@@ -31,9 +34,11 @@
   import { useModal } from '/@/components/Modal';
   import { getMaterialList } from '/@/api/personnel/material';
   import { BasicTable, useTable, TableAction } from '/@/components/Table';
+  import { jsonToSheetXlsx } from '/@/components/Excel';
+  import { transformData } from '../data';
 
   const [registerModal, { openModal }] = useModal();
-  const [registerTable, { reload }] = useTable({
+  const [registerTable, { reload, getRawDataSource }] = useTable({
     title: '物资申请列表',
     api: getMaterialList,
     columns,
@@ -63,5 +68,24 @@
   }
   function handleSuccess() {
     reload();
+  }
+  function customHeader() {
+    const { rows } = getRawDataSource();
+    const data = transformData(rows);
+    jsonToSheetXlsx({
+      data,
+      header: {
+        applicant: '申请人',
+        uphone: '手机号',
+        createdAt: '申请时间',
+        type: '物资类型',
+        content: '物资内容',
+        approver: '审批人',
+        status: '审批进度',
+        approve_time: '审批时间',
+        description: '审批意见',
+      },
+      filename: '物资申请列表.xlsx',
+    });
   }
 </script>
