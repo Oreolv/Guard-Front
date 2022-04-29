e19832044c374ac6ab43fed9bcbc3741272e3169
“Oreolv” <455001000@qq.com>
2022-04-29 21:04:09 +0800

    feat: 健康上报导出

--- src/views/personnel/health/index.vue
+++ src/views/personnel/health/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="m-4">
     <BasicTable @register="registerTable">
+      <template #toolbar>
+        <a-button @click="customHeader"> 导出列表 </a-button>
+      </template>
       <template #action="{ record }">
         <TableAction
           :actions="[
@@ -31,9 +34,11 @@
   import { useModal } from '/@/components/Modal';
   import { getHealthList } from '/@/api/personnel/health';
   import { BasicTable, useTable, TableAction } from '/@/components/Table';
+  import { jsonToSheetXlsx } from '/@/components/Excel';
+  import { transformData, transformForegin } from '../data';
 
   const [registerModal, { openModal }] = useModal();
-  const [registerTable, { reload }] = useTable({
+  const [registerTable, { reload, getRawDataSource }] = useTable({
     title: '健康上报列表',
     api: getHealthList,
     columns,
@@ -63,5 +68,33 @@
   }
   function handleSuccess() {
     reload();
+  }
+  function customHeader() {
+    const { rows } = getRawDataSource();
+    const data = transformData(rows);
+    data.map((d) => {
+      d.diagnosis = transformForegin(d.diagnosis);
+      d.contact = transformForegin(d.contact);
+      d.symptom = transformForegin(d.symptom);
+    });
+
+    jsonToSheetXlsx({
+      data,
+      header: {
+        applicant: '申请人',
+        uphone: '手机号',
+        createdAt: '申请时间',
+        temperature: '今日体温',
+        diagnosis: '是否为疑似、确诊患者',
+        contact: '是否接触疑似、确诊患者',
+        symptom: '是否有咳嗽、乏力等症状',
+        hospital: '是否就诊、住院',
+        approver: '审批人',
+        status: '审批进度',
+        approve_time: '审批时间',
+        description: '审批意见',
+      },
+      filename: '健康上报列表.xlsx',
+    });
   }
 </script>
